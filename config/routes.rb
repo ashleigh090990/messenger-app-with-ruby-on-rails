5da1bd5757ff9bc6910b98a2a29c36873d7659f8
Ashleigh Maund <ashleighmaund@gmail.com>
2018-03-21 08:51:28 +0000

    fixing home route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   devise_for :views
   devise_for :users
+  resources :conversations do
+  	resources :messages
+  end
+  root 'conversations#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
